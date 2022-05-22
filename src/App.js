d098df27dce636333f037812923ce956e16605f3
Ahmed <ahmedpasic15@gmail.com>
2022-05-22 13:51:01 +0200

    Added axios and the axios API fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import axios from "axios";
 
 function App() {
+  const API_KEY = "c4904ed7a4620e6f16fb7d1c33309261"
+
+ 
+  const newMovie = () => {
+    axios({
+      method : "GET",
+      url: `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`
+    }).then((response) => {
+      console.log(response)
+    })
+    
+  }
+
+
   return (
     <div>
+      <button onClick={newMovie}>New Movie</button>
       <div>Hello</div>
     </div>
   );
