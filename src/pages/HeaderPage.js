f902e8b7f890b2521d919071952c4ee06aee0f85
Ahmed <ahmedpasic15@gmail.com>
2022-05-23 00:00:26 +0200

    Styled header Banner form sm screens

--- src/pages/HeaderPage.js
+++ src/pages/HeaderPage.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function HeaderPage() {
   return (
     <div className='headerPage'>
       <div className='banner_wraper'>
-        Hello 
+        <h1>Unlimited films, TV programms and more</h1>
+        <p>Watch anywhere. Cancel anytime.</p>
+        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+        <Link className='trynow' to="/login">Get Started > </Link>
       </div>
     </div>
   )
