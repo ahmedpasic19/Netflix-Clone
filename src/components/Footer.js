e598336f8b5b399b7f7977e22515b48c7f31d9aa
Ahmed <ahmedpasic15@gmail.com>
2022-05-23 23:38:42 +0200

    Finished footer styling position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,34 @@
+import Line from "./Line";
+
 export default function Footer() {
   return (
     <div className="footer">
-      <ul className="footer_links">
-        <a>FAQ</a>
-        <a>Investor Relations</a>
-        <a>Privacy</a>
-        <a>Speed Test</a>
-      </ul>
-      <ul className="footer_links">
-        <a>Help Center</a>
-        <a>Jobs</a>
-        <a>Cookie Preferences</a>
-        <a>Legal Notices</a>
-      </ul>
-      <ul className="footer_links">
-        <a>Account</a>
-        <a>Ways to Watch</a>
-        <a>Corporate Information</a>
-        <a>Only on Netflix</a>
-      </ul>
-      <ul className="footer_links">
-        <a>Media Center</a>
-        <a>Terms of Use</a>
-        <a>Contact Us</a>
-      </ul>
+      <Line />
+      <div className="footer_content">
+        <ul className="footer_links">
+          <a>FAQ</a>
+          <a>Investor Relations</a>
+          <a>Privacy</a>
+          <a>Speed Test</a>
+        </ul>
+        <ul className="footer_links">
+          <a>Help Center</a>
+          <a>Jobs</a>
+          <a>Cookie Preferences</a>
+          <a>Legal Notices</a>
+        </ul>
+        <ul className="footer_links">
+          <a>Account</a>
+          <a>Ways to Watch</a>
+          <a>Corporate Information</a>
+          <a>Only on Netflix</a>
+        </ul>
+        <ul className="footer_links">
+          <a>Media Center</a>
+          <a>Terms of Use</a>
+          <a>Contact Us</a>
+        </ul>
+      </div>
     </div>
   );
 }
