f9a17597936e9713fb872c2f0dd11359b2eb2ecd
Ahmed <ahmedpasic15@gmail.com>
2022-05-24 02:01:57 +0200

    Finished SignUP Page Coppyed Login Page layout need to make functional use FireBase

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,18 @@
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className="login">
+      <div className="form_wraper">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign Un</button>
+        </form>
+        <p>
+        Already have an account? <a href="/login" className="font-bold hover:underline underline-offset-2">Log In</a>
+        </p>
+      </div>
+    </div>
   )
 }
