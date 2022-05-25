b51b0705c5220c1943fd684e8db8702b24bab2fa
Ahmed <ahmedpasic15@gmail.com>
2022-05-25 19:50:06 +0200

    Updated NavigationBar component Rearanged the layout Set LG and SM navabrs Made the 2 navbars responsive need to make MENU button functional

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,37 +4,27 @@ import logo from "../nefliximages/Logonetflix.png";
 
 import { auth } from "../firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
-import Dropdown from "./Dropdown";
-
 
 export default function NavigationBar() {
-  const [dropDownOpen, setdropDownOpen] = useState(false)
   const [showLink, setshowLink] = useState("nav_Link")
   const [showBtn, setshowBtn] = useState()
- 
-  const openMenu = () => {
-    (dropDownOpen) ? setdropDownOpen(false) : setdropDownOpen(true)
-  }
+
   
-  
+  // CHECKS IF USER IS LOGGED AND SHOWS/HIDES lg NAVBAR LINKS
   onAuthStateChanged(auth,(currentUser) => {
     if(currentUser) {
       setshowLink("nav_Link")
+      setshowBtn("resBtn")
     } else {
       setshowLink("hidden")
+      setshowBtn("hidden")
     }
   })
-
-  // HAMBUREGER MENU 
-const dropdownMenu = () => {
-  for(let i = 0; i >= 1 ; i++) {
-    return <Dropdown />
-  }
-}
-
   
   // REGULAR NAVIGATION BAR
   return (
+    <div>
+      {/* NAVBAR FOR lg SCRENS */}
     <div className="navbar">
       <Link className="logo_wraper" to="/browse" >
         <img src={logo} alt="logo" className="logo"/>
@@ -46,8 +36,28 @@ const dropdownMenu = () => {
         <Link to="/latest" className={showLink}>Latest</Link>
         <Link to="/mylist" className={showLink}>MyList</Link>
       </ul>
-      <button className="resBtn" onClick={openMenu}>Menu</button>
-      {(dropDownOpen) ? dropdownMenu() : console.log("menu CLOSED")}
+      <p>LG navbar</p>
+      <button className={showBtn}>Menu</button>
+    </div>
+
+    {/* NAVBAR FOR sm SCREENS */}
+    <div className="sm_navbar">
+      <Link className="logo_wraper" to="/browse" >
+        <img src={logo} alt="logo" className="logo"/>
+      </Link>
+      <ul className="nav_links">
+        <Link to="/browse" className={showLink}>Home</Link>
+        <Link to="/TVshows" className={showLink}>Tv Shows</Link>
+        <Link to="/movies" className={showLink}>Movies</Link>
+        <Link to="/latest" className={showLink}>Latest</Link>
+        <Link to="/mylist" className={showLink}>MyList</Link>
+      </ul>
+      <p>SM navbar</p>
+      <button className={showBtn}>Menu</button>
+    </div>
+
+
+
     </div>
   );
 }
